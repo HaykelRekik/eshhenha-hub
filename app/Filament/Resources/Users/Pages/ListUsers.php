f9b6de1314b51c9e3e9eb65d63a44ad375f2660a
Haykel Rekik <hayk.rekik@gmail.com>
2025-08-18 22:25:33 +0100

    fix: correct spelling of resolveRole method in HasRole trait

--- app/Filament/Resources/Users/Pages/ListUsers.php
+++ app/Filament/Resources/Users/Pages/ListUsers.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace App\Filament\Resources\Users\Pages;
 
+use App\Enums\UserRole;
 use App\Filament\Resources\Users\UserResource;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Schemas\Components\Tabs\Tab;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListUsers extends ListRecords
 {
@@ -16,6 +19,26 @@ class ListUsers extends ListRecords
     {
         return [
             CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make(__('All Users'))
+                ->icon('phosphor-users-duotone'),
+
+            'admins' => Tab::make(__('Admins'))
+                ->icon(UserRole::ADMIN->getIcon())
+                ->modifyQueryUsing(fn(Builder $query): Builder => $query->role(UserRole::ADMIN)),
+
+            'companies' => Tab::make(__('Companies'))
+                ->icon(UserRole::COMPANY->getIcon())
+                ->modifyQueryUsing(fn(Builder $query): Builder => $query->role(UserRole::COMPANY)),
+
+            'users' => Tab::make(__('Customers'))
+                ->icon(UserRole::USER->getIcon())
+                ->modifyQueryUsing(fn(Builder $query): Builder => $query->role(UserRole::USER)),
         ];
     }
 }
