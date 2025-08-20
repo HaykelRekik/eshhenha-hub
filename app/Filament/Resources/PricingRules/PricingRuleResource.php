f3d4705968a0c9e1c22bda30abc40047ec876675
Haykel Rekik <hayk.rekik@gmail.com>
2025-08-20 07:40:05 +0100

    feat: add pricing rules (wip)

---
+++ app/Filament/Resources/PricingRules/PricingRuleResource.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Filament\Resources\PricingRules;
+
+use App\Filament\Resources\PricingRules\Pages\CreatePricingRule;
+use App\Filament\Resources\PricingRules\Pages\EditPricingRule;
+use App\Filament\Resources\PricingRules\Pages\ListPricingRules;
+use App\Filament\Resources\PricingRules\Schemas\PricingRuleForm;
+use App\Filament\Resources\PricingRules\Tables\PricingRulesTable;
+use App\Models\PricingRule;
+use BackedEnum;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Table;
+
+class PricingRuleResource extends Resource
+{
+    protected static ?string $model = PricingRule::class;
+
+    protected static string|BackedEnum|null $navigationIcon = null;
+
+    protected static ?int $navigationSort = 3;
+
+    public static function form(Schema $schema): Schema
+    {
+        return PricingRuleForm::configure($schema);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return PricingRulesTable::configure($table);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListPricingRules::route('/'),
+            'create' => CreatePricingRule::route('/create'),
+            'edit' => EditPricingRule::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getNavigationGroup(): ?string
+    {
+        return __('Shipments Management');
+    }
+
+    public static function getLabel(): ?string
+    {
+        return __('Pricing Rule');
+    }
+
+    public static function getPluralLabel(): ?string
+    {
+        return __('Pricing Rules');
+    }
+}
